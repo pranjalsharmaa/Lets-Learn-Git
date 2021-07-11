9349e5bf1f0ebca8aed8196b2f69e7ab43a8fe04
Pranjal Sharma <pranjalsharmavds@gmail.com>
2021-07-11 08:44:40 +0530

    adding Diff.java commit

---
+++ Diff.java
@@ -0,0 +1,9 @@
+public class Diff {
+    public static void main(String[] args) {
+        int a=30;
+        int b=20;
+        int c= a-b;
+        System.out.println("The sum is:" +  c);
+    }
+    
+}
