9e96dc7c6ed3ee36c7664ff38675e3e25d3f3b9c
Pranjal Sharma <pranjalsharmavds@gmail.com>
2021-07-11 09:03:14 +0530

    added Multiply.java, both modified

--- Multiply.java
+++ Multiply.java
@@ -1,8 +1,8 @@
-public class Diff {
+public class Multiply {
     public static void main(String[] args) {
         int a=30;
         int b=20;
-        int c= a-b;
+        int c= a*b;
         System.out.println("The diff is:" +  c);
     }
     
--- Sum.java
+++ Sum.java
@@ -6,6 +6,7 @@ public class Sum {
     public static void main(String[] args) {
         int a=30;
         int b=20;
-        System.out.println(a+b);
+        int c =a+b;
+        System.out.println("The sum is:" +  c);
     }
 }
