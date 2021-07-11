f22968dc4a542babaca90ffe60f3910caa81a242
Pranjal Sharma <pranjalsharmavds@gmail.com>
2021-07-11 09:23:28 +0530

    added implementation

--- Diff.java
+++ Diff.java
@@ -1,9 +1,9 @@
 public class Diff {
     public static void main(String[] args) {
         int a=30;
-        int b=20;
-        int c= a-b;
-        System.out.println("The diff is:" +  c);
+        int b=560;
+        
+        System.out.println("The diff is:" +  (a-b));
     }
     
 }
